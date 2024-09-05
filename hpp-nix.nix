957a85976c75e8e1f8e03005146db032066a34c0
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-05 15:33:03 +0200

    more python3Packages

--- hpp-nix.nix
+++ hpp-nix.nix
@@ -1,14 +1,14 @@
 {
   stdenv,
-  hpp-corbaserver,
-  hpp-gepetto-viewer,
-  hpp-manipulation-corba,
   hpp-task-sequencing,
   lib,
-  python3,
+  python3Packages,
   rsync,
 }:
-python3.pkgs.toPythonModule (stdenv.mkDerivation {
+let
+  inherit (python3Packages.python) sitePackages;
+in
+python3Packages.toPythonModule (stdenv.mkDerivation {
   pname = "hpp-nix";
   version = "0.0.1";
 
@@ -16,16 +16,26 @@ python3.pkgs.toPythonModule (stdenv.mkDerivation {
   dontUnpack = true;
 
   propagatedBuildInputs = [
-    python3.pkgs.numpy
-    python3.pkgs.omniorbpy
+    python3Packages.numpy
+    python3Packages.omniorbpy
   ];
 
-
   installPhase = ''
-    mkdir -p $out/${python3.sitePackages}
-    ${lib.getExe rsync} -a ${hpp-corbaserver.out}/${python3.sitePackages}/ $out/${python3.sitePackages}
-    ${lib.getExe rsync} -a ${hpp-gepetto-viewer.out}/${python3.sitePackages}/ $out/${python3.sitePackages}
-    ${lib.getExe rsync} -a ${hpp-manipulation-corba.out}/${python3.sitePackages}/ $out/${python3.sitePackages}
-    ${lib.getExe rsync} -a ${hpp-task-sequencing.out}/${python3.sitePackages}/ $out/${python3.sitePackages}
+    mkdir -p $out/${sitePackages}
+    for pkg in \
+      ${python3Packages.hpp-baxter.out} \
+      ${python3Packages.hpp-corbaserver.out} \
+      ${python3Packages.hpp-environments.out} \
+      ${python3Packages.hpp-gepetto-viewer.out} \
+      ${python3Packages.hpp-gui.out} \
+      ${python3Packages.hpp-manipulation-corba.out} \
+      ${python3Packages.hpp-practicals.out} \
+      ${python3Packages.hpp-romeo.out} \
+      ${hpp-task-sequencing.out} \
+      ${python3Packages.hpp-tutorial.out} \
+      ${python3Packages.hpp-universal-robot.out}
+    do
+        ${lib.getExe rsync} -a $pkg/${sitePackages}/ $out/${sitePackages}
+    done
   '';
 })
